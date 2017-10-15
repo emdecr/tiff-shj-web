a1eacdaf88f97d0b6841fe41de67f5304ca875da
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-10-15 16:54:28 -0400

    Adds progress footer animations

--- src/js/init.js
+++ src/js/init.js
@@ -1,7 +1,7 @@
 //Init WOW.js with animate.css
 new WOW().init();
 
-//use window.scrollY
+// Show/Hide Nav and Footer
 var scrollpos = window.scrollY;
 var topNav = document.getElementById("top-nav");
 var progressFooter = document.getElementById("progress-footer");
@@ -22,6 +22,41 @@ function remove_footer_class_on_scroll() {
     progressFooter.classList.remove("show");
 }
 
+var line_to_goals           = document.getElementById("progress-line--to-goals");
+var line_to_impressions     = document.getElementById("progress-line--to-impressions");
+var line_to_media           = document.getElementById("progress-line--to-media");
+var line_to_next            = document.getElementById("progress-line--to-next");
+
+function progress_grow_on_scroll(el) {
+    el.classList.add("grow");
+}
+
+function progress_shrink_on_scroll(el) {
+    el.classList.remove("grow");
+}
+
+function isScrolledIntoView(el) {
+    scrollpos = window.scrollY;
+    var elemTop = el.getBoundingClientRect().top;
+    var elemBottom = el.getBoundingClientRect().bottom;
+
+    // var min = (window.innerHeight-100)*(-1);
+    var min = window.innerHeight;
+    console.log(min);
+
+    var isVisible = (elemTop <= (min-100)) && (elemBottom >= 0);
+    console.log(scrollpos+' '+isVisible+' '+elemTop+' '+elemBottom);
+    return isVisible;
+    // document.body.scrollHeight;
+}
+
+// Sections
+var goalsSupporters = document.getElementById("results-goals-supporters");
+var impressions     = document.getElementById("results-impressions");
+var media           = document.getElementById("results-media");
+var whatsNext       = document.getElementById("results-whats-next");
+
+// Scroll Events
 window.addEventListener('scroll', function(){ 
     //Here you forgot to update the value
     scrollpos = window.scrollY;
@@ -41,4 +76,33 @@ window.addEventListener('scroll', function(){
         remove_footer_class_on_scroll();
     }
 
+    if (isScrolledIntoView(goalsSupporters) == true) {
+        progress_grow_on_scroll(line_to_goals)
+    } else {
+        progress_shrink_on_scroll(line_to_goals)
+    }
+
+    if (isScrolledIntoView(impressions) == true) {
+        progress_grow_on_scroll(line_to_impressions)
+        progress_grow_on_scroll(line_to_goals)
+    } else {
+        progress_shrink_on_scroll(line_to_impressions)
+    }
+
+    if (isScrolledIntoView(media) == true) {
+        progress_grow_on_scroll(line_to_impressions)
+        progress_grow_on_scroll(line_to_goals)
+        progress_grow_on_scroll(line_to_media)
+    } else {
+        progress_shrink_on_scroll(line_to_media)
+    }
+
+    if (isScrolledIntoView(whatsNext) == true) {
+        progress_grow_on_scroll(line_to_impressions)
+        progress_grow_on_scroll(line_to_goals)
+        progress_grow_on_scroll(line_to_media)
+        progress_grow_on_scroll(line_to_next)
+    } else {
+        progress_shrink_on_scroll(line_to_next)
+    }
 });
